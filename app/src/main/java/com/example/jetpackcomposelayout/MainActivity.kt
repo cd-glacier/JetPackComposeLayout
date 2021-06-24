2b688b46af8f56d29f09657226da1aedeba7e53f
cd-glacier <hyoga0216@gmail.com>
2021-06-25 00:53:25 +0900

    User Scaffold and TopAppBar

--- app/src/main/java/com/example/jetpackcomposelayout/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposelayout/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
@@ -35,7 +37,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LayoutsCodelab() {
-    Text(text = "Hi there!")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "LayoutsCodelab") },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(modifier = Modifier
+            .padding(innerPadding)
+            .padding(8.dp))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
 }
 
 @Preview
