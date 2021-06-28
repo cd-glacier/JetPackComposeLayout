ccd5f682f9b800f085031f487889b6961ae759c0
cd-glacier <hyoga0216@gmail.com>
2021-06-28 14:58:44 +0900

    Exceeded divider

--- app/src/main/java/com/example/jetpackcomposelayout/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposelayout/MainActivity.kt
@@ -50,6 +50,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(
+            color = Color.Black, modifier = Modifier
+                .fillMaxHeight()
+                .width(1.dp)
+        )
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    LayoutsCodelabTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
+    }
+}
+
+@Composable
 fun DecoupledConstraintLayout() {
     BoxWithConstraints {
         val constraints = if (maxWidth < maxHeight) {
@@ -84,7 +120,6 @@ private fun decoupledConstraints(margin: Dp): ConstraintSet {
         }
     }
 }
-
 
 @Preview
 @Composable
